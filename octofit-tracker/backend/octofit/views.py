7b542e93db96597358d4b1fba47d041e15f1d51e
Ma. Anna Carigaba <69285545+Mdcarigaba@users.noreply.github.com>
2025-04-08 08:30:47 +0000

    updated views.py

--- octofit-tracker/backend/octofit/views.py
+++ octofit-tracker/backend/octofit/views.py
@@ -5,15 +5,15 @@ from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, Lea
 from django.http import JsonResponse
 
 def api_root(request):
-    base_url = "https://organic-space-sniffle-vwwxpq65pqx2pwp6-8000.app.github.dev"
+    base_url = "https://organic-space-sniffle-vwwxpq65pqx2pwp6-8000.app.github.dev/api/v1"
     return JsonResponse({
         "message": "Welcome to the Octofit API!",
         "urls": {
-            "users": "https://organic-space-sniffle-vwwxpq65pqx2pwp6-8000.app.github.dev/users/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
-            "teams": "https://organic-space-sniffle-vwwxpq65pqx2pwp6-8000.app.github.dev/teams/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
-            "activities": "https://organic-space-sniffle-vwwxpq65pqx2pwp6-8000.app.github.dev/activities/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
-            "leaderboard": "https://organic-space-sniffle-vwwxpq65pqx2pwp6-8000.app.github.dev/leaderboard/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
-            "workouts": "https://organic-space-sniffle-vwwxpq65pqx2pwp6-8000.app.github.dev/workouts/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
+            "users": f"{base_url}/users/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
+            "teams": f"{base_url}/teams/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
+            "activities": f"{base_url}/activities/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
+            "leaderboard": f"{base_url}/leaderboard/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
+            "workouts": f"{base_url}/workouts/", # CODESPACE NAME: organic-space-sniffle-vwwxpq65pqx2pwp6
         },
     })
 
